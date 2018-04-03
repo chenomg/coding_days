f2edef2ea5bcd40b6b14296912c4ec9f2587331b
chenomg <xxmm@live.cn>
2018-04-03 23:40:36 +0800

    minus plus is needed to be fixed

--- other cases/AplusB/AplusB.py
+++ other cases/AplusB/AplusB.py
@@ -16,20 +16,30 @@ a和b都是 32位 整数么？
 """
 
 
-def aplusb(a, b):
-    c = a ^ b
-    d = (a & b) << 1
-    if d == 0:
-        return c
-    return aplusb(c, d)
+class Solution(object):
+    def aplusb(self, a, b):
+        if a >= 0 and b >=0:
+            c = a ^ b
+            d = (a & b) << 1
+            if d == 0:
+                return c
+            return self.aplusb(c, d)
+        elif a < 0 and b >= 0:
+            minus_a = self.aplusb(~a, 1)
+            c = self.aplusb(minus_a, b)
+            return c
+        elif b < 0 and a >= 0:
+            minus_b = self.aplusb(~b, 1)
+            c = self.aplusb(minus_b, a)
+            return c
 
-
-def main():
-    a_int = 121
-    b_int = 82
-    result_value = aplusb(a_int, b_int)
-    print(result_value)
+    def main(self):
+        a_int = 121
+        b_int = 82
+        result_value = self.aplusb(a_int, b_int)
+        print(result_value)
 
 
 if __name__ == "__main__":
-    main()
+    solution = Solution()
+    solution.main()
--- other cases/AplusB/test_AplusB.py
+++ other cases/AplusB/test_AplusB.py
@@ -1,20 +1,38 @@
-import unittest
-import AplusB
+import pytest
+from AplusB.AplusB import Solution
 
 
-class testAplusB(unittest.TestCase):
-    def test1_123p456(self):
+class Test_AplusB:
+    Testsolution = Solution()
+
+    def test_123p456(self):
         """
         AplusB函数测试
         """
-        self.assertEqual(579, AplusB.aplusb(123, 456))
+        assert 579 == self.Testsolution.aplusb(123, 456)
 
-    def test2_5299p8456(self):
+    def test_5299p8456(self):
         """
         AplusB函数测试
         """
-        self.assertEqual(13755, AplusB.aplusb(5299, 8456))
+        assert 13755 == self.Testsolution.aplusb(5299, 8456)
 
+    def test_0p123(self):
+        """
+        AplusB函数测试
+        """
+        assert 123 == self.Testsolution.aplusb(0, 123)
 
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    def test_10pn123(self):
+        """
+        AplusB函数测试
+        10 + (-123) = -113
+        """
+        assert -113 == self.Testsolution.aplusb(10, -123)
+
+    def test_100pn100(self):
+        """
+        AplusB函数测试
+        100 + (-100) = 0
+        """
+        assert 0 == self.Testsolution.aplusb(100, -100)
