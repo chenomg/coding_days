ac395dd0f4817a703343d06b9808a5f0efc7d693
Jase Chen <xxmm@live.cn>
2018-04-08 23:17:53 +0800

    create case04: english file added, words_calculate.py added

---
+++ case04/words_calculate.py
@@ -0,0 +1,25 @@
+file_name = 'Why People Leave the Church and Never Come Back.txt'
+
+
+def words_calculate(file_name):
+    """TODO: Calculate the number of words in the file.
+
+    :file_name: type: string, the name of the file in the same direction
+    :returns: type: string, the number of words in the file
+
+    """
+    with open(file_name, 'r') as f:
+        f.readline()
+        # print(f.read())
+
+
+def main():
+    """main function
+    :returns: None
+
+    """
+    words_calculate(file_name)
+
+
+if __name__ == "__main__":
+    main()
