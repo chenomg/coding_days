9b450e6bc121363fc816fa4fd4b62b03b098a2b1
Jase Chen <xxmm@live.cn>
2018-04-09 12:47:02 +0800

    feature added: words_counter --> return the number of words in the text file

--- case04/words_calculate.py
+++ case04/words_calculate.py
@@ -3,16 +3,21 @@ import re
 file_name = 'Why People Leave the Church and Never Come Back.txt'
 
 
-def words_calculate(file_name):
-    """TODO: Calculate the number of words in the file.
+class Words_Calculate(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-    :file_name: type: string, the name of the file in the same direction
-    :returns: type: string, the number of words in the file
+    def words_calculate(self):
+        """TODO: Calculate the number of words in the file.
 
-    """
-    with open(file_name, 'r') as f:
-        f.readline()
-        # print(f.read())
+        :file_name: type: string, the name of the file in the same direction
+        :returns: type: string, the number of words in the file
+
+        """
+        with open(file_name, 'r') as f:
+            f_read = f.read()
+            words_count_list = re.split(r'\W+', f_read)
+        return len(words_count_list)
 
 
 def main():
@@ -20,7 +25,9 @@ def main():
     :returns: None
 
     """
-    words_calculate(file_name)
+    test = Words_Calculate(file_name)
+    words_count = test.words_calculate()
+    print(words_count)
 
 
 if __name__ == "__main__":
