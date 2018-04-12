77ee6beff94a3485e569c42b00e75639b4d51ec3
Jase Chen <xxmm@live.cn>
2018-04-12 23:56:10 +0800

    脚本根据指定大小对目录下面过大的图片进行缩小，并在文件名前增加 thumbnail 以区分

--- case05/pic_resize.py
+++ case05/pic_resize.py
@@ -9,63 +9,81 @@ Description: 第 0005 题： 你有一个目录，装了很多照片，把它们
 """
 
 from PIL import Image
-from math import floor
 import os
 import re
 
 
-def image_path_list(path):
-    image_name_list = []
-    image_path_list = []
-    file_list = os.listdir(path)
-    for f in file_list:
-        if not os.path.isdir(f):
-            if re.findall(r'(\.jp(|e)g|\.png)$', f, re.IGNORECASE):
-                image_name_list.append(f)
-                image_path_list.append(os.path.join(path, f))
-        else:
-            pass
-    return image_path_list
+class pic_resize(object):
+    """
+    Description
 
+    @param param: 脚本目录下pic目录中的图片若长宽尺寸超出最大尺寸则进行缩小，
+        不超过的则不变
+    @type  param: Type
 
-def image_zoomout_save(image, max_width=1136, max_height=640):
-    p = re.compile(r'^\/.+\/')
-    path = p.findall(image)
-    file_name = re.findall(r'\w+\.\w+$', image)
-    new_name = 'thumbnail-' + file_name[0]
-    output_file = os.path.join(path[0], new_name)
-    img = Image.open(image)
-    width, height = img.size
-    if width > max_width or height > max_height:
-        if width / height > max_width / max_height:
-            rate = width / max_width
-            img.thumbnail((floor(width / rate), floor(height / rate)))
-            img.save(output_file, 'jpeg')
-            print("Process done")
-        else:
-            rate = height / max_height
-            img.thumbnail((floor(width / rate), floor(height / rate)))
-            img.save(output_file, 'jpeg')
-            print("Process done")
-    else:
-        print("Picture doesn't need to zoomout")
+    @return: Description
+    @rtype: Type
+
+    @raise e: Description
+
+    """
+
+    def __init__(
+            self,
+            # 初始化参数
+            # 获取脚本所在目录，并且生成图片所在目录
+            path=os.path.join(os.getcwd(), 'pic'),
+            max_width=1136,
+            max_height=640):
+        self.path = path
+        self.max_width = max_width
+        self.max_height = max_height
+        self.image_names_list = []
+        self.image_path_name_list = []
+
+    def complete_image_path_name_list_and_names_list(self):
+        """
+        生成需要进行缩放处理图片的文件名清单和文件完整目录(带文件名)的清单
+        """
+        image_names_list = []
+        image_path_name_list = []
+        file_list = os.listdir(self.path)
+        for f in file_list:
+            if not os.path.isdir(f):
+                if re.findall(r'(^(?!thumbnail)).*(\.jp(|e)g|\.png)$', f,
+                              re.IGNORECASE):
+                    img = Image.open(os.path.join(self.path, f))
+                    width, height = img.size
+                    if width > self.max_width or height > self.max_height:
+                        image_names_list.append(f)
+                        image_path_name_list.append(os.path.join(self.path, f))
+            else:
+                pass
+        self.image_path_name_list = image_path_name_list
+        self.image_names_list = image_names_list
+
+    def image_zoomout_saver(self):
+        """
+        根据前面生成的需要处理的文件清单进行缩小处理，
+        并在缩小的文件名前面增加thumbnail以区分
+        """
+        self.complete_image_path_name_list_and_names_list()
+        for image in self.image_path_name_list:
+            p = re.compile(r'^\/.+\/')
+            path = p.findall(image)[0]
+            path_length = len(path)
+            file_name = image[path_length + 1:]
+            new_name = 'thumbnail-' + file_name
+            output_file = os.path.join(path, new_name)
+            img = Image.open(image)
+            img.thumbnail((self.max_width, self.max_width))
+            img.save(output_file, 'png')
+            print("Process done: {}".format(image))
 
 
 def main():
-    addr = '/Users/chenomg/code/coding_days/case05/pic/'
-    # img_addr = '/Users/chenomg/code/coding_days/case05/pic/banner.jpg'
-    image_files = []
-    # files = file_list(addr)
-    image_files = image_path_list(addr)
-    print(image_files)
-    # image = Image.open(image_files[0])
-    # image.show()
-    path = re.findall(r'^\/.+\/', image_files[0])
-    print(image_files[0])
-    print(path)
-    # print(lambda  for f in image_files[0])
-    print(f for f in image_files[0])
-    image_zoomout_save(image_files[5])
+    work = pic_resize()
+    work.image_zoomout_saver()
 
 
 if __name__ == "__main__":
