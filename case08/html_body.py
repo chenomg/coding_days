960fde3787b01f6f8d4db955a762008a0c7a4e5e
Jase Chen <xxmm@live.cn>
2018-05-06 23:44:11 +0800

    get the content of web page, and save it

---
+++ case08/html_body.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+# =============================================================================
+#      FileName: html_body.py
+#          Desc: 获取简书网页正文的内容，并保存到文件
+#        Author: Jase Chen
+#         Email: xxmm@live.cn
+#      HomePage: http://jase.im/
+#       Version: 0.0.1
+#    LastChange: 2018-05-06 21:06:37
+#       History:
+# =============================================================================
+'''
+import requests
+from lxml import html
+
+# 获取网页文章正文的内容保存文件
+file_name = '/Users/chenomg/code/My_codes/coding_days/case08/crawl.txt'
+headers = {
+    'user-agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
+}
+# 获取网页
+response = requests.get(
+    'https://www.jianshu.com/p/3ea8262b0927', headers=headers)
+# 网页转换为lxml可用
+selector = html.fromstring(response.text)
+with open(file_name, 'w') as f:
+    # 获取相应正文处的内容
+    for sel in selector.xpath('//div[@class="show-content"]/*/p/text()'):
+        # 保存内容
+        f.write(sel.strip() + '\n')
+print('Processing done...')
